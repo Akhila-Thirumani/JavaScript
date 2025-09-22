273580bd7ef20e09f8bfc72ca85da5e10d3d901b
akhila <akhilathirumani42@gmail.com>
2025-09-22 11:02:05 +0530

    datatype and also variable

--- app.js
+++ app.js
@@ -1,7 +1,38 @@
-var a=10;
-var b=30;
-let c=54;
+//var a=10;
+//var b=30;
+//let c=54;
 
-console.log(a);
-console.log(b);
-console.log(`value of b is ${b}`);
+//console.log(a);
+//console.log(b);
+//console.log(`value of b is ${b}`);
+
+
+// know the type of data "this window" command 
+//primitive
+a=2020;
+console.log(typeof(a));
+
+b="akhila";
+console.log(typeof(b));
+
+c=true;
+console.log(typeof(c))
+data=null;
+console.log(typeof(data));
+
+d=undefined;
+console.log(typeof(d));
+
+//non-primitives
+array=[20,"akhi",30,40];
+console.log(typeof(array));
+
+obj={
+    name:"akhila",
+    age:20,
+
+};
+console.log(typeof(obj));
+
+
+
